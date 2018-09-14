a0bf0f06b26b56db86eb276d3a049731fe1e4afc
Filype Ferreira <filypef@gmail.com>
2018-09-13 23:01:49 -0300

    add bubbling btn remove on card0

--- js/card.js
+++ js/card.js
@@ -7,21 +7,47 @@
         //criando a constante 
         const card = cards[i];
 
+        //adiciona classe cartao focado
         function cardFocusIn(){
             card.classList.add('cartao--focado');
         }
 
+        //remove classe cartao focado
         function cardFocusOut(){
             card.classList.remove('cartao--focado');
         }
 
+        //adiciona escutador focusin no cartao
         card.addEventListener('focusin', cardFocusIn);
+
+        //adiciona escutador focusout no cartao
         card.addEventListener('focusout', cardFocusOut);
 
+        //adiciona bubbling no cartao para opcoes de colors
         card.addEventListener('change', function(e){
             if(e.target.classList.contains('opcoesDoCartao-radioTipo')){
                 const color = e.target.value;
                 card.style.backgroundColor = color;
+            }
+        })
+
+        //Adiciona classe some na div para remover
+        function addClassCard(btnRemove){
+            btnRemove.parentElement.parentElement.classList.add('cartao--some');
+        }
+
+        //Remove o cartão
+        function removeCard(btnRemove){
+            setTimeout(() => {
+                btnRemove.parentElement.parentElement.remove();
+            },400)
+        }
+
+        //bubbling no cartao para botao remove
+        card.addEventListener('click', function(e){
+            if(e.target.classList.contains('opcoesDoCartao-remove')){
+                addClassCard(e.target);
+                removeCard(e.target);
             }
         })
     }
